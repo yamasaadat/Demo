54908bf521e61ca20d92f061dda45047d5a126da
George Davis <george_davis@trendmicro.com>
2020-06-05 02:48:34 +0000

    Commenting file upload code for the demo, scratch code added to fileRouter for the timebeing

--- services/file-storage/file-storage.js
+++ services/file-storage/file-storage.js
@@ -1,13 +1,52 @@
 const fs = require('fs');
+/*const multer = require('multer');
 
-exports.uploadFile = (file) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+exports.uploadFile = (req, res) => {
+ //   console.log("Upload File: " + req);
+   // console.log(JSON.stringify(req.file));
+    //console.log(process.cwd());
+
+    let upload = multer({ storage: storage });
 
     return new Promise(function(resolve, reject) {
         
+	upload.single('upload'), function(req, res, next) {
+	        // req.file contains information of uploaded file
+        	// req.body contains information of text fields, if there were any
+
+	        if (req.fileValidationError) {
+        	    return res.send(req.fileValidationError);
+        	}		
+	        else if (!req.file) {
+	            return res.send('Please select an image to upload');
+	        }
+	        else if (err instanceof multer.MulterError) {
+	            return res.send(err);
+	        }
+	        else if (err) {
+	            return res.send(err);
+	        }
+
+        	// Display uploaded image for user validation
+	        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+    	};
+
         fs.readFile(file, function (err, data) {
 
             if (err) {
-                console.log("There was an error")
+                console.log("There was an error");
+		console.log(err); 
                 reject(false);
             } else {    
 
@@ -25,4 +64,4 @@ exports.uploadFile = (file) => {
             }
         });
     });
-};
+};*/
