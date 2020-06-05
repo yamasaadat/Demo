c1f73bc01396bb28593c0565cb2e213150c58fb8
George Davis <george_davis@trendmicro.com>
2020-06-05 02:45:35 +0000

    Adding file upload functionality with multer (draft)

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+var upload = multer({ storage: storage });
+
 const s3Client = require('../services/s3/s3.js');
 const fileClient = require('../services/file-storage/file-storage.js');
 // const azBlobCient = require('../services/azure-blob/azure-blob.js');
@@ -13,13 +26,31 @@ router.post('/fileUploadToS3', function(req, res, next) {
   });
 });
 
-router.post('/fileUploadToServer', function(req, res, next) {
-  return fileClient.uploadFile(req.body.upload).then (function (result) {
+router.post('/fileUploadToServer', upload.single('upload'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.json(file);
+  
+})
+
+/*
+ * router.post('/fileUploadToServer', function(req, res, next) {
+  //console.log(req.body);
+  //console.log(req.body);
+  //console.log(req.body.upload);
+  return fileClient.uploadFile(req, res).then (function (result) {
     res.send('File Uploaded. Thank you.');
   }, function (err) {
+    console.log(err);
     res.send('Upload failed. Try again.');
   });
 });
+*
+*/
 
 // router.post('/fileUploadToAzBlob', function(req, res, next) {
 //   if(azBlobClient.uploadFile(req.body.upload)) {
